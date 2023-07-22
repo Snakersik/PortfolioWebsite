f3ec3908bec57e86ef0754c5bdc7b799d81033b6
Snakersik <konradtratwal@gmail.com>
2023-07-22 11:17:53 +0200

    ProbablyFinish

--- script.js
+++ script.js
@@ -12,14 +12,17 @@ function scrollToSection4() {
   document.querySelector(".section4").scrollIntoView({ behavior: "smooth" });
 }
 
+function scrollToSection5() {
+  document.querySelector(".section5").scrollIntoView({ behavior: "smooth" });
+}
 
-window.addEventListener(
-  "wheel",
-  function (event) {
-    event.preventDefault();
-  },
-  { passive: true }
-);
+const toggle = document.querySelector(".toogle-button");
+const dropDownMenu = document.querySelector(".dropdown-menu");
+
+toggle.onclick = function () {
+  console.log("click");
+  dropDownMenu.classList.toggle("open");
+};
 
 window.addEventListener("scroll", function () {
   var nav = document.querySelector("nav");
@@ -31,34 +34,3 @@ window.addEventListener("scroll", function () {
     nav.classList.remove("scrolled");
   }
 });
-
-const buttons = document.querySelectorAll("[data-carousel-button]")
-const theme = document.querySelector(".project-title");
-
-
-buttons.forEach(button => {
-  button.addEventListener("click", () => {
-    const offset = button.dataset.carouselButton === "next" ? 1 : -1
-    const slides = button
-      .closest("[data-carousel]")
-      .querySelector("[data-slides]")
-
-    const activeSlide = slides.querySelector("[data-active]")
-    let newIndex = [...slides.children].indexOf(activeSlide) + offset
-    if (newIndex < 0) newIndex = slides.children.length - 1
-    if (newIndex >= slides.children.length) newIndex = 0
-
-    slides.children[newIndex].dataset.active = true
-    delete activeSlide.dataset.active
-    if (newIndex === 0) {
-        theme.textContent = "E-commerce shop";
-      } else if (newIndex === 1) {
-        theme.textContent = "Snake game";
-      } else if (newIndex === 2) {
-        theme.textContent = "Project 3";
-      }
-
-    
-
-  })
-})
